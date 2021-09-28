19db0c7f7436e2994426b7007f621a3fec39120f
Sondre <Sondre.Gjesdal@gmail.com>
2021-09-28 15:33:06 +0200

    first iteration of put, post, get, delete for todos. Along with its class

--- counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
+++ counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
@@ -1,11 +1,11 @@
 package no.hvl.dat110.rest.counters;
 
-import static spark.Spark.after;
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.put;
-
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 public class App {
 	
 	static Counters counters = null;
+	static List<Todo> todoList = null;
 	
 	public static void main(String[] args) {
 
@@ -24,6 +25,7 @@ public class App {
 		}
 
 		counters = new Counters();
+		todoList = new ArrayList<Todo>();
 		
 		after((req, res) -> {
   		  res.type("application/json");
@@ -48,6 +50,52 @@ public class App {
             return counters.toJson();
         	
         });
+
+        get("/todo", (request, response) -> {
+        	Gson gson = new Gson();
+
+        	String jsonString = gson.toJson(todoList);
+
+        	return jsonString;
+		});
+
+        put("/todo/*", (request, response) -> {
+        	Gson gson = new Gson();
+
+        	int index = Integer.parseInt(request.splat()[0])-1;
+
+        	Todo todo = todoList.get(index);
+
+        	Todo todoUpdate = gson.fromJson(request.body(), Todo.class);
+
+        	todo.setDescription(todoUpdate.getDescription());
+        	todo.setSummary(todoUpdate.getSummary());
+
+        	return gson.toJson(todo);
+		});
+
+        post("/todo/*", (request, response) -> {
+        	Gson gson = new Gson();
+
+        	Todo todo = gson.fromJson(request.body(), Todo.class);
+
+        	todoList.add(todo);
+
+        	return gson.toJson(todo);
+		});
+
+        delete("/todo/*", (request, response) -> {
+        	Gson gson = new Gson();
+
+        	String[] splat = request.splat();
+
+        	Integer in = Integer.parseInt(splat[0]);
+
+        	todoList.remove(in-1);
+
+        	return gson.toJson(todoList);
+		});
+
     }
     
 }
